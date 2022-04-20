4831947c1c458592e36fe031e6fbec845637c092
Eunsung Moon <eunsung9647@gmail.com>
2022-04-20 20:02:11 +0900

    별찍기

--- star.js
+++ star.js
@@ -1,10 +1,15 @@
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
-    const n = data.split(" ");
-    const a = Number(n[0]), b = Number(n[1]);
-    const row = '*'.repeat(a)
-    for(let i =0; i < b; i++){
-        console.log(row)
-    }
+  const n = data.split(" ");
+  const a = Number(n[0]), b = Number(n[1]);
+  const row = '*'.repeat(a)
+  for (let i = 0; i < b; i++) {
+    console.log(row)
+  }
 
 });
+
+/*
+a : 가로 한줄 채우기
+b : 세로 줄 채우기
+*/
