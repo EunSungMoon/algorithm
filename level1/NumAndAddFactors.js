42e681294281c3c471e80dc6713f4276fbc34256
Eunsung Moon <eunsung9647@gmail.com>
2022-05-18 21:34:39 +0900

    약수의 개수와 덧셈

---
+++ level1/NumAndAddFactors.js
@@ -0,0 +1,16 @@
+function solution(left, right) {
+  let answer = 0;
+  for (let i = left; i <= right; i++) {
+    if (Number.isInteger(Math.sqrt(i))) {
+      answer -= i;
+    } else {
+      answer += i;
+    }
+  }
+  return answer;
+}
+
+/*
+제곱근이 정수면 약수의 개수는 홀수
+
+*/
