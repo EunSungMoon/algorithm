2ff48e3c47a4c60f784d4ffcb5cb9c7d14a6e462
Eunsung Moon <eunsung9647@gmail.com>
2022-06-03 00:01:07 +0900

    올바른 괄호

---
+++ level2/correctParenthesis.js
@@ -0,0 +1,14 @@
+function solution(s){
+  let cum = 0
+  for (let paren of s) {
+      cum += paren === '('? 1: -1
+      if(cum < 0) {
+          return false
+      }
+  }
+  return cum === 0? true: false;
+}
+
+/*
+다시
+*/
