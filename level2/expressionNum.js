9b16bd38a0ac3fcc84a0125dc687e613004571ce
Eunsung Moon <eunsung9647@gmail.com>
2022-06-09 11:33:39 +0900

    숫자의 표현

--- level2/expressionNum.js
+++ level2/expressionNum.js
@@ -9,6 +9,27 @@ function solution(n) {
   return answer;
 }
 
+function solution(n) {
+  let answer = 0;
+  let start = 1;
+
+  while (start <= n) {
+    let tmp = 0;
+
+    for (let i = start; tmp <= n; i++) {
+      tmp += i;
+
+      if (tmp === n) {
+        answer++;
+      }
+    }
+
+    start++;
+  }
+
+  return answer;
+}
+
 /*
 주어진 자연수를 연속된 자연수의 합으로 표현하는 방법의 수와 주어진 수의 홀수인 약수 갯수와 같다
 는 공식을 활용한 풀이
