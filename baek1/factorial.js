5615c69689c43e8c7bca106abf2007d6c98f7922
Eunsung Moon <eunsung9647@gmail.com>
2022-06-22 21:53:12 +0900

    팩토리얼

--- baek1/factorial.js
+++ baek1/factorial.js
@@ -1,12 +1,20 @@
+// function solution(num) {
+//   function factorial(a) {
+//     if (a <= 1) {
+//       return 1;
+//     }
+//     return a * factorial(a - 1);
+//   }
+
+//   console.log(factorial(num));
+// }
+
 function solution(num) {
-  function factorial(a) {
-    if (a <= 1) {
-      return 1;
-    }
-    return a * factorial(a - 1);
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i
   }
-  
-  console.log(factorial(num));
+  console.log(result);
 }
 
 solution(5)
